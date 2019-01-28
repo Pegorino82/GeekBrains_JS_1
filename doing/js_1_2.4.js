7df04274f14adaa5eadf7e1a69026bf3fddf2309
pegorino <eshkryabin@yandex.ru>
2019-01-28 17:05:55 +0300

    lesson 2 done

---
+++ doing/js_1_2.4.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function addTwo(a, b) {
+    /**
+     * функция складывает два числа
+     */
+    return a + b
+}
+
+function subTwo(a, b) {
+    /**
+     * функция вычитает из числа а число b
+     */
+    return a - b
+}
+
+function mulTwo(a, b) {
+    /**
+     * функция перемножает два числа
+     */
+    return a * b
+}
+
+function divTwo(a, b) {
+    /**
+     * функция делит a на b
+     */
+    return a / b
+}
