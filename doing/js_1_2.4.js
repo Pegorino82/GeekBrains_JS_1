bb74f234c8b538937c1e08e59492d58f5d911549
pegorino <eshkryabin@yandex.ru>
2019-01-28 17:36:33 +0300

    поправил доки для функций

--- doing/js_1_2.4.js
+++ doing/js_1_2.4.js
@@ -1,29 +1,41 @@
 "use strict";
 
+/**
+ * функция складывает два числа
+ * @param a number
+ * @param b number
+ * @returns {number} сумма a и b
+ */
 function addTwo(a, b) {
-    /**
-     * функция складывает два числа
-     */
     return a + b
 }
 
+/**
+ * функция вычитает из числа а число b
+ * @param a number
+ * @param b number
+ * @returns {number} разность a и b
+ */
 function subTwo(a, b) {
-    /**
-     * функция вычитает из числа а число b
-     */
     return a - b
 }
 
+/**
+ * функция перемножает два числа
+ * @param a number
+ * @param b number
+ * @returns {number} произведение a и b
+ */
 function mulTwo(a, b) {
-    /**
-     * функция перемножает два числа
-     */
     return a * b
 }
 
+/**
+ * функция делит a на b
+ * @param a number
+ * @param b number
+ * @returns {number} частное a и b
+ */
 function divTwo(a, b) {
-    /**
-     * функция делит a на b
-     */
     return a / b
 }
