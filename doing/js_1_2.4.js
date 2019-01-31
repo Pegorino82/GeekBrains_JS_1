d034e425e5a53d17e59d630e0fe8de8b080338a3
pegorino <eshkryabin@yandex.ru>
2019-01-31 14:09:58 +0300

    correction of mistakes

--- doing/js_1_2.4.js
+++ doing/js_1_2.4.js
@@ -2,8 +2,8 @@
 
 /**
  * функция складывает два числа
- * @param a number
- * @param b number
+ * @param {number} a
+ * @param {number} b
  * @returns {number} сумма a и b
  */
 function addTwo(a, b) {
@@ -12,8 +12,8 @@ function addTwo(a, b) {
 
 /**
  * функция вычитает из числа а число b
- * @param a number
- * @param b number
+ * @param {number} a
+ * @param {number} b
  * @returns {number} разность a и b
  */
 function subTwo(a, b) {
@@ -22,8 +22,8 @@ function subTwo(a, b) {
 
 /**
  * функция перемножает два числа
- * @param a number
- * @param b number
+ * @param {number} a
+ * @param {number} b
  * @returns {number} произведение a и b
  */
 function mulTwo(a, b) {
@@ -32,8 +32,8 @@ function mulTwo(a, b) {
 
 /**
  * функция делит a на b
- * @param a number
- * @param b number
+ * @param {number} a
+ * @param {number} b
  * @returns {number} частное a и b
  */
 function divTwo(a, b) {
